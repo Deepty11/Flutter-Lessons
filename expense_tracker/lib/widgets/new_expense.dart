d47d5b0d35584eaadf29362f0c076b47fe3d59b2
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2024-02-28 00:43:13 +0600

    expense_tracker: * Add Dropdown menu for categories * Add Calender * Dismiss modal manually using Navigator

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,6 +1,10 @@
+import 'package:expense_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return _NewExpenseState();
@@ -10,6 +14,33 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _dateSelected;
+  Category? _selectedCategory = Category.leisure;
+
+  void _presentCalender() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    var pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+
+    setState(() {
+      _dateSelected = pickedDate;
+    });
+  }
+
+  void _onSelectCategory(Category? value) {
+    if (value == null) {
+      return;
+    }
+
+    setState(() {
+      _selectedCategory = value;
+      print(_selectedCategory);
+    });
+  }
 
   @override
   void dispose() {
@@ -26,23 +57,70 @@ class _NewExpenseState extends State<NewExpense> {
         children: [
           TextField(
             controller: _titleController,
-            decoration: const InputDecoration(
-              label: Text('Title'),
-            ),
-          ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              prefixText: '\$',
-              label: Text('Amount'),
+            decoration: InputDecoration(
+              label: Text(
+                'Title',
+                style: GoogleFonts.lato(fontSize: 14),
+              ),
             ),
           ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: '\$',
+                    label: Text(
+                      'Amount',
+                      style: GoogleFonts.lato(fontSize: 14),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _dateSelected == null
+                          ? 'Select Date'
+                          : formatter.format(_dateSelected!),
+                    ),
+                    IconButton(
+                      onPressed: _presentCalender,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map((category) => DropdownMenuItem(
+                            value: category,
+                            child: Text(
+                              category.name.toUpperCase(),
+                            ),
+                          ))
+                      .toList(),
+                  onChanged: _onSelectCategory),
+              const Spacer(),
               TextButton(
                 onPressed: () {
-                  print('Canceled');
+                  Navigator.pop(context);
                 },
                 child: const Text('Cancel'),
               ),
